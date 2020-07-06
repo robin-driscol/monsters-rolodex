63b25fab6914d9f24645a7e4f09349f9a18b24d0
Andromeda <andromeda@Andromedas-Mac.local>
2020-07-06 19:35:18 +0600

    Updates

---
+++ precache-manifest.445e2dc4926812bceaca0b05cfa2ff06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a28b75e91e67b4a1004a20b342d722",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8264610311fd01a1a24",
+    "url": "/monsters-rolodex/static/css/main.d9151207.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8264610311fd01a1a24",
+    "url": "/monsters-rolodex/static/js/main.8bfa996e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
